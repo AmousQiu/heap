87255a4b9409fdbfbc74cccb8f141dc7068de9bf
Ziyu Qiu <571459556@qq.com>
2018-11-19 21:57:48 -0400

    Update ClientDemo.java

--- ClientDemo.java
+++ ClientDemo.java
@@ -1,5 +1,30 @@
-public class ClientDemo {
-    public static void main(String args[]){
+import java.io.*;
+import java.util.Scanner;
 
+public class ClientDemo {
+    public static void main(String args[]) throws IOException{
+        Heap<Process> memory=new Heap<Process>();
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter the filename:");
+        String filename=keyboard.nextLine();
+        File file = new File(filename);
+        Scanner inputFile = new Scanner(file);
+        int id;
+        int timeRead;
+        int priority;
+        int timeArrival;
+        while(inputFile.hasNext()){
+            id=inputFile.nextInt();
+            timeRead=inputFile.nextInt();
+            priority=inputFile.nextInt();
+            timeArrival=inputFile.nextInt();
+            Process p=new Process(id,timeRead,priority,timeArrival);
+            memory.add(p);
+        }
+        int timeCount=1;//mean the time unit
+        while(!memory.isEmpty()){
+            System.out.println("Time Unit:"+timeCount);
+            System.out.println();
+        }
     }
 }
